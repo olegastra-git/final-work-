0ff3add0282b7730f815a100891f9a2ad03c21eb
Alekseev Oleg <qwertyray1995@gmail.com>
2023-02-09 23:33:04 +0300

    заработало

--- Program.cs
+++ Program.cs
@@ -23,3 +23,24 @@ string[] FindLessThan(string[] input, int n)
 
     return output;
 }
+
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray()
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
