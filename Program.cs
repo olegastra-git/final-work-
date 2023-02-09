437e886f654a2a5bcad49b4cd5de7fc160bcd753
Alekseev Oleg <qwertyray1995@gmail.com>
2023-02-09 23:32:00 +0300

    условия

--- Program.cs
+++ Program.cs
@@ -2,3 +2,24 @@
 using static System.Console;
 
 Clear();
+
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n)
+{
+    string[] output = new string[CountLessThan(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
